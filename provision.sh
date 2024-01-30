a86adb89df144f813c4d9858a23c0c28ea65fabf
Lisa Overall <11458176+lisaoverall@users.noreply.github.com>
2024-01-30 02:53:47 -0500

    infra: Make sure all afl-* binaries are on the vagrant user's PATH.
    Signed-off-by: Lisa Overall <11458176+lisaoverall@users.noreply.github.com>

--- provision.sh
+++ provision.sh
@@ -10,17 +10,14 @@ cd /vagrant
 if ! [ -d AFL ]; then 
 	echo "Cloning and building AFL"
 	git clone https://github.com/google/AFL.git
-	cd AFL/
-	make && make install
-	cd - 
 fi
+make -C AFL && make -C AFL install
 
 if ! [ -d afl-cov ]; then 
 	echo "Cloning afl-cov"
 	git clone https://github.com/mrash/afl-cov.git
 fi
-
-export PATH=$PATH:/vagrant/afl-cov
+echo "export PATH=$PATH:/vagrant/afl-cov" >> /home/vagrant/.bashrc
 
 if ! [ -f heartbleed/openssl-1.0.1f.tar.gz ]; then
 	echo "Fetching OpenSSL source containing Heartbleed vulnerability"
